2ca0b739409e6ebbac5d0b88b5d478b0aec1c8eb
Vikas Mishra <64024001+vikas886@users.noreply.github.com>
2021-01-10 00:44:06 +0530

    first commit

---
+++ test/widget_test.dart
@@ -0,0 +1,12 @@
+
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:mychat_app/main.dart';
+
+void main() {
+   testWidgets('Checking if hello world shows up', (WidgetTester tester) async {
+    // Build our app and trigger a frame.
+    await tester.pumpWidget(MyApp());
+    expect(find.text('Hello World!'), findsOneWidget);
+  });
+}
